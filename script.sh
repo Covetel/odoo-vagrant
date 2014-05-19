2abe341944d2c406aa4be1d51de7fea334bd14c5
kamihacker <kamihacker@yahoo.com>
2014-05-19 13:40:56 -0500

    creating vars for values that vary from site to site

--- script.sh
+++ script.sh
@@ -1,14 +1,24 @@
 #!/bin/bash 
 
+# var definitions - change depending on your infrastructure
+MIRROR_HOSTNAME=mirror
+MIRROR_IP=10.0.2.2
+PORT=3142
+DEB_MIRROR=$MIRROR_HOSTNAME:$PORT
+
+
+
 # mirror ip address
-sudo echo "10.0.2.2 mirror" >> /etc/hosts
+sudo echo "$MIRROR_IP $MIRROR_HOSTNAME" >> /etc/hosts
 
 # apt sources
-sudo echo "deb http://mirror:3142/ftp.debian.org/debian/ wheezy main contrib non-free" > /etc/apt/sources.list
-sudo echo "deb http://mirror:3142/nightly.openerp.com/7.0/nightly/deb/ ./" >> /etc/apt/sources.list
+sudo echo "deb http://$DEB_MIRROR/ftp.debian.org/debian/ wheezy main contrib non-free" > /etc/apt/sources.list
+sudo echo "deb http://$DEB_MIRROR/nightly.odoo.com/7.0/nightly/deb/ ./" >> /etc/apt/sources.list
 
-# update
+#let's get the latest catalog of packages
 sudo apt-get update
+#let's get the latest version of software on the OS
+sudo apt-get upgrade 
 
 # Install packages
 sudo apt-get -y install debconf-utils less lsof screen vim git openerp
